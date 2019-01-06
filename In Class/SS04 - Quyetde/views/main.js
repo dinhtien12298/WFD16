b107e9ecd0444d58d80765e294d1581a597ab46f
DESKTOP-LG083FR\Dinh Tien <dinhtiennguyen.1202@gmail.com>
2019-01-06 23:58:51 +0700

    complete - can bo sung mongoDB

--- In Class/SS04 - Quyetde/views/main.js
+++ In Class/SS04 - Quyetde/views/main.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "http://localhost:5000/randomquestion",
+    url: "/randomquestion",
     type: "GET",
     success: function(data){
         $("#question").text(data.question.content);
@@ -12,18 +12,25 @@ $.ajax({
 
 $("#no, #yes").on("click", function(){
     $.ajax({
-        url: "http://localhost:5000/answer",
+        url: "/answer",
         type: "POST",
         data: {
             questionId: $("#question").attr("data-question"),
             vote: $(this).attr("id")
         },
         success: function(data) {
-            console.log(data);
-            window.location.href = `http://localhost:5000/question/${data.question.id}`;
+            window.location.href = `/result/${data.question.id}`;
         },
         error: function(err) {
             console.log(err);
         }
     });
 });
+
+$("#result").on("click", function() {
+    window.location.href = `/result/${$("#question").attr("data-question")}`;
+})
+
+$("#otherQuestion").on("click", function() {
+    window.location.href = `/`;
+})
